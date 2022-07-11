9b27a89cc108fb556f5e5d2d810a64f1b3cd8ddf
YashRRamteke <102619396+YashRRamteke@users.noreply.github.com>
2022-07-12 00:41:47 +0530

    Update cdf_plot.py

--- Code/cdf_plot.py
+++ Code/cdf_plot.py
@@ -1,75 +1,38 @@
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy as sp
-import math
 import mpmath as mp
-import functions as fp
+import scipy
+import matplotlib.pyplot as plt
 
-#if using termux
-# import subprocess
-# import shlex
-#end if
+def qfunc(x):
+    return (0.5)*mp.erfc(x/np.sqrt(2))
 
+qfunc_vec = scipy.vectorize(qfunc, otypes=['double'])
 
-maxrange=30
-maxlim=4.0
+maxrange=50
+maxlim=6.0
 x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
-xx = np.linspace(0,maxlim,maxrange*5) #more points
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
+pdf = [] #declaring pdf list
+h = 2*maxlim/(maxrange-1);
 #randvar = np.random.normal(0,1,simlen)
-# randvar = np.loadtxt('uni.dat',dtype='double')
-# randvar = np.loadtxt('gau.dat',dtype='double')
-randvar = np.loadtxt('ray.dat',dtype='double')
+#randvar = np.loadtxt('uni.dat',dtype='double')
+randvar = np.loadtxt('gau.dat',dtype='double')
+
 for i in range(0,maxrange):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
-
-
-vec_unit_cdf = np.vectorize(fp.unit_cdf, otypes=[np.float])
-vec_gau_cdf = np.vectorize(fp.gau_cdf, otypes=[np.float])
-vec_log_cdf = np.vectorize(fp.log_cdf, otypes=[np.float])
-vec_tri_cdf = np.vectorize(fp.tri_cdf, otypes=[np.float])
-vec_q_gau_cdf = np.vectorize(fp.q_gau_cdf, otypes=[np.float])
-vec_chi_cdf = np.vectorize(fp.chi_cdf, otypes=[np.float])
-vec_ray_cdf = np.vectorize(fp.ray_cdf, otypes=[np.float])
-
-a = []
-
-for i in range(1,11):
-	a.append(i*0.1)
-
-
-
-
-plt.semilogy(np.array(a), np.loadtxt('proberr_graph.dat',dtype='double'),'o') #plotting proberr graph
-plt.semilogy(xx,vec_q_gau_cdf(xx))
 	
+for i in range(0,maxrange-1):
+	test = (err[i+1]-err[i])/(x[i+1]-x[i])
+	pdf.append(test) #storing the pdf values in a list
 
-
-
-
-# plt.plot(x.T, err, 'o')#plotting the CDF
-# plt.plot(xx,vec_unit_cdf(xx)) #plotting theoretical unit CDF
-# plt.plot(xx,vec_gau_cdf(xx)) #plotting theoretical gaussian CDF
-# plt.plot(xx,vec_log_cdf(xx)) #plotting theoretical logarithmic CDF
-# plt.plot(xx, vec_tri_cdf(xx))
-# plt.plot(xx, vec_chi_cdf(xx))
-# plt.plot(xx, vec_ray_cdf(xx))
+plt.plot(x,err,'.')
+plt.plot(x, np.ones(maxrange) - qfunc_vec(x))
 plt.grid() #creating the grid
-plt.xlabel('$A$')
-plt.ylabel('$P_e$')
-plt.legend(["Numerical","Theory"])
-
-#if using termux
-# plt.savefig('../figs/uni_cdf.pdf')
-# plt.savefig('../figs/uni_cdf.eps')
-# subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
-#if using termux
-#plt.savefig('../figs/gauss_cdf.pdf')
-#plt.savefig('../figs/gauss_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
-#else
-plt.show() #opening the plot window
+plt.xlabel('$x_i$')
+plt.ylabel('$F_X(x_i)$')
+plt.legend(["Simulation", "Analysis"])
+plt.savefig('./figs/2_2.png')
